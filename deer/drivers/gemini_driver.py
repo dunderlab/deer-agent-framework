7feaf9c1f285ab8921d78ecfc6342f2c59078f76
Yeison <yencardonaal@unal.edu.co>
2026-05-18 13:06:59 -0500

    New LLM calls basics

--- deer/drivers/gemini_driver.py
+++ deer/drivers/gemini_driver.py
@@ -1,4 +1,3 @@
-import re
 import json
 from typing import TypeVar, Type
 
@@ -15,28 +14,53 @@ class GeminiDriver(LLMDriver):
         self.client = genai.Client()
         self.model_name = model_name
 
-    def generate(self, prompt: str, response_model: Type[T] = None) -> T:
+    #
+    # def generate(self, prompt: str, response_model: Type[T] = None, mime_type: str = "application/json") -> T:
+    #
+    #     response = self.client.models.generate_content(
+    #         model=self.model_name,
+    #         contents=prompt,
+    #         config={
+    #             "response_mime_type": mime_type,
+    #         },
+    #     )
+    #
+    #     if mime_type == "application/json":
+    #         response_text = response.text
+    #         response_text = escape_logic(response_text)
+    #         data = json.loads(response_text)
+    #     else:
+    #         data = response.text
+    #
+    #     if response_model is None:
+    #         return data
+    #
+    #     return response_model.model_validate(data)
 
+    def generate_text(self, prompt: str) -> str:
+        response = self.client.models.generate_content(
+            model=self.model_name,
+            contents=prompt,
+            config={
+                "response_mime_type": "text/plain",
+            },
+        )
+        return response.text
+
+    def generate_json(self, prompt: str, response_model: Type[T] = None) -> T:
         response = self.client.models.generate_content(
             model=self.model_name,
             contents=prompt,
             config={
                 "response_mime_type": "application/json",
-                # "response_schema": response_model.model_json_schema(),
             },
         )
 
         response_text = response.text
-        response_text = escape_logic(response_text)
-
+        response_text = self.escape_logic(response_text)
         data = json.loads(response_text)
 
-        if response_model is None:
-            return data
+        if response_model:
+            data = response_model.model_validate(data)
 
-        return response_model.model_validate(data)
-
-
-def escape_logic(text: str) -> str:
-    text = text.replace('"""', '\\"\\"\\"')
-    return text
+        return data
