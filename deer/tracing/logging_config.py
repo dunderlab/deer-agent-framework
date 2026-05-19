07817aa0674b75bad75e0759afcc28d4c1762c95
Yeison <yencardonaal@unal.edu.co>
2026-05-19 15:25:17 -0500

    Added logger

---
+++ deer/tracing/logging_config.py
@@ -0,0 +1,17 @@
+import logging
+
+LOGGER_NAME = "DEER"
+
+logger = logging.getLogger(LOGGER_NAME)
+
+if not logger.handlers:
+    handler = logging.StreamHandler()
+
+    formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+
+logger.setLevel(logging.DEBUG)
+logger.propagate = False
