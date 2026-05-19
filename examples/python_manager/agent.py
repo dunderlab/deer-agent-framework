d72752e15853eb15e2849f80c6b0f9723309f855
Yeison <yencardonaal@unal.edu.co>
2026-05-19 18:29:36 -0500

    Example

--- examples/python_manager/agent.py
+++ examples/python_manager/agent.py
@@ -1,15 +1,16 @@
 import logging
 
 from deer.core.agent import DeterministicAgent
-from deer.drivers.gemini_driver import GeminiDriver
+from deer.drivers import GeminiDriver, OllamaDriver
 
 from tools import registry
 from dotenv import load_dotenv
 
 load_dotenv()
-logging.getLogger("DEER").setLevel(logging.WARNING)
+logging.getLogger("DEER").setLevel(logging.DEBUG)
 
 driver = GeminiDriver(model_name="gemini-3.1-flash-lite")
+# driver = OllamaDriver(model_name="qwen2.5-coder:7b")
 agent = DeterministicAgent(
     identity="You are a senior Python packaging and environment management specialist with deep expertise in"
     "pyproject.toml, dependency management, reproducible environments, build systems, virtual environments, "
@@ -18,16 +19,16 @@ agent = DeterministicAgent(
     registry=registry,
     format_response="markdown",
 )
-# agent.repl()
+agent.repl()
 
 
-if __name__ == "__main__":
-    chain_messages = [
-        "Quiero que generes un archivo llamado 'example.py' con ejemplo de un script en python que imprima un 'Hola mundo'.",
-        "Quiero que edites el archivo 'example.py' que agreges un función llamada 'main'.",
-    ]
-
-    for message in chain_messages:
-        print(f">>> {message}\n")
-        response = agent.send(message)
-        print(f"    {response}\n\n")
+# if __name__ == "__main__":
+#     chain_messages = [
+#         "Quiero que generes un archivo llamado 'example.py' con ejemplo de un script en python que imprima un 'Hola mundo'.",
+#         "Quiero que edites el archivo 'example.py' que agreges un función llamada 'main'.",
+#     ]
+#
+#     for message in chain_messages:
+#         print(f">>> {message}\n")
+#         response = agent.send(message)
+#         print(f"    {response}\n\n")
