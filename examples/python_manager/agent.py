58f33cc82dfb56ee36f99278ba8279bd86626115
Yeison <yencardonaal@unal.edu.co>
2026-05-21 21:25:13 -0500

    Improve example

--- examples/python_manager/agent.py
+++ examples/python_manager/agent.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 
 from deer.core.agent import DeterministicAgent
 from deer.drivers import GeminiDriver, OllamaDriver
@@ -7,7 +8,9 @@ from tools import registry
 from dotenv import load_dotenv
 
 load_dotenv()
-logging.getLogger("DEER").setLevel(logging.DEBUG)
+
+logger = logging.getLogger("DEER")
+logger.setLevel(logging.DEBUG)
 
 # driver = GeminiDriver(model_name="gemini-3.1-flash-lite")
 # driver = GeminiDriver(model_name="gemini-1.5-flash-lite")
@@ -21,20 +24,24 @@ agent = DeterministicAgent(
     registry=registry,
     format_response="markdown",
     max_tries_for_plan=5,
+    rollback=None,
 )
 # agent.repl()
 
 
 if __name__ == "__main__":
     chain_messages = [
-        "Quiero que generes un archivo llamado 'example.py' con ejemplo de un script en python que imprima un 'Hola mundo'.",
-        "Quiero que edites el archivo 'example.py' que agreges un función llamada 'main'.",
-        "Write 'Secret Code 789' to a file named 'secret.txt'",
+        # "Quiero que generes un archivo llamado 'example.py' con ejemplo de un script en python que imprima un 'Hola mundo'.",
+        # "Quiero que edites el archivo 'example.py' que agreges un función llamada 'main'.",
+        "Just write 'Secret Code 789' to a file named 'secret.txt'",
+        # "Quiero que generes un proyecto python completo llamado 'pynguino', con su 'pyproject.toml', "
+        # "sus directorios de tests, sau modulo principal, con los siguientes submodulos: 'utils', 'core', 'api', "
+        # "quiero que generes el archivo de requerimientos con 'numpy', 'requests' y 'pandas', quiero que generes archivos python con"
+        # "funciones placeholder para mi proyecto. Mi proyecto consiste en un log para API externa."
     ]
 
-    agent.generate_chat_log(chain_messages)
-    #
-    # for message in chain_messages:
-    #     print(f">>> {message}\n")
-    #     response = agent.send(message)
-    #     print(f"    {response.text()}\n\n")
+    agent.generate_chat_log(
+        chain_messages,
+        print_chat=True,
+        save_log=f"log-{datetime.now().timestamp()}.log",
+    )
