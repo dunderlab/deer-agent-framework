83e41eadc1371fdfe2997ddf2ec7d85255033847
Yeison <yencardonaal@unal.edu.co>
2026-05-21 21:22:34 -0500

    Shared formatter

--- deer/tracing/logging_config.py
+++ deer/tracing/logging_config.py
@@ -1,19 +1,19 @@
 import logging
 
+formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+
 logger = logging.getLogger("DEER")
 if not logger.handlers:
     handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.WARNING)
 logger.propagate = False
 
 
 logger_llm = logging.getLogger("DEER-LLM")
 if not logger_llm.handlers:
     handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
     handler.setFormatter(formatter)
     logger_llm.addHandler(handler)
 logger_llm.setLevel(logging.WARNING)
